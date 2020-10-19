7be2b6f03288791962361ab3d58e9e9a6c136bfb
AlejandroOrtizBalmis <57657339+AlejandroOrtizBalmis@users.noreply.github.com>
2020-10-19 18:57:26 +0200

    Add files via upload

---
+++ Ejercicio6/Ejercicio6/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio6
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Image_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ((sender as Image).Opacity == 1) (sender as Image).Opacity = 0.5;
+            else (sender as Image).Opacity = 1;
+
+        }
+    }
+}
